a40126b05aeb238910d28ae3da1e7cccbb8abd38
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-06-03 18:12:02 -0400

    Andrew and Musa_School3_solution

--- classes/school.py
+++ classes/school.py
@@ -1,5 +1,7 @@
 from classes.staff import Staff
 from classes.student import Student
+import csv
+import os
 
 class School:
     def __init__(self, name):
@@ -16,3 +18,36 @@ class School:
         for student in self.students:
             if student.school_id == student_id:
                 return student
+
+    def add_student(self,student_data):
+        self.students.append(Student(**student_data))
+        self.save()
+
+    def delete_student(self,id):
+        for student in self.students:
+            if id == student.school_id:
+                self.students.remove(student)
+        self.save()
+
+    def save(self):
+        output = []
+        for student in self.students:
+            output.append([student.name,student.age,student.role,student.school_id,student.password])
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "../data/students.csv")
+
+        with open(path,'w') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['name','age','role','school_id','password'])
+            csvwriter.writerows(output)
+
+        
+
+# name,age,role,school_id,password
+# Lisa,25,Student,13345,xx 
+# Jessie,25,Student,12335,xx
+# Slater,25,Student,12645,xx
+# kim,31,Student,34456,xx
+# dave,77,Student,788908,xx
+# doug,88,Student,0809890,xx
+
